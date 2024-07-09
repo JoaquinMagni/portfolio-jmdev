97eefd96f9b41904fc994da4a5dea955def3cb4e
Joaquin A. Magni <joaquinagueromagni@gmail.com>
2024-07-09 13:55:02 -0300

    enviar mensajes en el formulario

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -4,17 +4,9 @@ import { Button } from "../../components/ui/button";
 import { Input } from "../../components/ui/input";
 import { Textarea } from "../../components/ui/textarea"
 
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue
-} from "../../components/ui/select"
-
 import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa"
+
+import Swal from 'sweetalert2'
 
 const info = [
   {
@@ -37,6 +29,34 @@ const info = [
 import { motion } from "framer-motion";
 
 const Contact = () => {
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "b33f2544-7e03-445b-95f6-f29aa0d9d6e5");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+      Swal.fire({
+        title: "¡Perfecto!",
+        text: "Su mensaje se ha enviado correctamente.",
+        icon: "success"
+      });
+    }
+  };
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -50,42 +70,34 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* Form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+            <form onSubmit={onSubmit} className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-accent">Concretemos tu proyecto juntos.</h3>
               <p className="text-white/60">
               Completa el formulario a continuación para que podamos ponernos en contacto.
               </p>
               {/* Input */}
               <div className="grid grid-col-1 md:grid-cols-2 gap-6">
-                <Input type="firstname" placeholder="Nombre" />
-                <Input type="lastname" placeholder="Apellido" />
-                <Input type="email" placeholder="Email" />
-                <Input type="phone" placeholder="Teléfono" />
+                <Input type="firstname" name="name" required placeholder="Nombre" />
+                <Input type="lastname" name="lastname" required placeholder="Apellido" />
+                <Input type="email" name="email" required placeholder="Email" />
+                <Input type="phone" name="phone" required placeholder="Teléfono" />
               </div>
-              {/* Select */}
-              <Select>
-                <SelectTrigger className="w-full">
-                  <SelectValue placeholder="Selecciona un servicio."/>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>-- Selecciona un servicio --</SelectLabel>
-                    <SelectItem value="est">Desarrollo Web</SelectItem>
-                    <SelectItem value="cst">Diseño UI/UX</SelectItem>
-                    <SelectItem value="mst">Diseño de Logo</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
               {/* Text Area */}
               <Textarea 
+                name="message"
+                required
                 className="h-[200px]"
                 placeholder="Escribe tu mensaje aquí."
               />
               {/* Button */}
-              <Button size="md" className="max-w-40">
+              <Button type="submit" size="md" className="max-w-40">
                 Enviar Mensaje
               </Button>
             </form>
+
+            <script>
+
+            </script>
           </div>
           {/* Info */}
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
